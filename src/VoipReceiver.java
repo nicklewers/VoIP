2db58cd8d3c04e21b5bcd22fe1e3ce11904e6d89
mmjjoozz <jozwiak385@gmail.com>
2018-02-18 11:57:44 +0000

    Fixed the output issue.

--- src/VoipReceiver.java
+++ src/VoipReceiver.java
@@ -21,9 +21,11 @@ public class VoipReceiver implements Runnable {
     //Port to open socket on.
     int PORT;
     //Vector to store audio blocks. 
-    Vector voiceVector;
+    Vector<byte[]> voiceVector;
     //Player to play audio blocks.
-    final AudioPlayer player;
+    AudioPlayer player;
+    //Set to false while running to terminate receiving and recording.
+    boolean running = true;
     
 
     /*
@@ -34,7 +36,7 @@ public class VoipReceiver implements Runnable {
     public VoipReceiver(int port) throws LineUnavailableException {
         this.PORT = port;
         this.player = new AudioPlayer();
-        this.voiceVector = new Vector();
+        this.voiceVector = new Vector<byte[]>();
     }
     
     //Create thread for receiver.
@@ -43,43 +45,59 @@ public class VoipReceiver implements Runnable {
         thread.start();
     }
     
+    /*
+    *Replay data received.
+    *@throws IOException if no blocks to play.
+    */
+    public void replay() throws IOException {
+        Iterator<byte[]> voiceItr = voiceVector.iterator();
+        
+        while(voiceItr.hasNext()){
+            player.playBlock(voiceItr.next());
+        }
+        
+        player.close();
+    }
+    
+    public void stop(){
+        running = false;
+    }
+    
     @Override
     public void run() {
         
         try {
-        //Open socket to receive to.
-        receiving_socket = new DatagramSocket(PORT);
-        //Iterator to simultaneously add and play received data.
-        Iterator<byte[]> voiceItr;
-        //Keep on receiving packets until false.
-        boolean running = true; 
- 
-            //512 bytes per block, pre-configured in AudioRecorder.
-            byte[] buffer = new byte[512];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            //Open socket to receive to.
+            receiving_socket = new DatagramSocket(PORT);
+            
+            //Init, fill, receive and play in the loop.
+            byte[] buffer;
+            DatagramPacket packet;
             
             while(running){
                 
-                //Loop running, receive.
+                //Keep initializing new buffer and receiving while running.
+                buffer = new byte[512];
+                packet = new DatagramPacket(buffer, 0, buffer.length);
                 receiving_socket.receive(packet);
-                
-                //Indicate received packet.
-                System.out.println("Packet received.");
-                
-                //Extract audio data from packet, add to vector.
+
+                //Add to use in replay() and count number of packets received.
                 voiceVector.add(packet.getData());
                 
-                //Iterate through received audio blocks.
-                voiceItr = voiceVector.iterator();
-                if(voiceItr.hasNext()){
-                    //Play each block received.
-                    player.playBlock(voiceItr.next());
-                }
+                //Play.
+                player.playBlock(packet.getData());
+                    
             }
             //Running is false, close player.
             player.close();
             //Quit receiving.
             receiving_socket.close();
+            System.out.printf("Receiver is closed. Received %d packets.\n", voiceVector.size());
+            
+            //Uncomment to replay, used for some troubleshooting earlier,
+            //kinda useless now.
+            //replay();
+            
         } catch (IOException e){
             e.printStackTrace();
         }
