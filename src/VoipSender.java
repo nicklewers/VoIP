31a2f5c97d26bbf8dca41fac862b22e4dce95050
mmjjoozz <jozwiak385@gmail.com>
2018-02-18 11:58:24 +0000

    Added some stuff, largely the same.

--- src/VoipSender.java
+++ src/VoipSender.java
@@ -2,10 +2,12 @@
 package voip;
 
 import CMPC3M06.AudioRecorder;
+import CMPC3M06.AudioPlayer;
 
 import javax.sound.sampled.LineUnavailableException;
 import java.io.IOException;
 
+import java.util.Iterator;
 import java.util.Vector;
 import java.net.*;
 
@@ -23,6 +25,9 @@ public class VoipSender implements Runnable {
     InetAddress clientIP;
     //Recorder to record audio to.
     final AudioRecorder recorder;
+    //Set to false while running to terminate sending and recording.
+    boolean running = true;
+    Vector<byte[]> voiceVector;
     
     /*
     * Construct sender.
@@ -41,10 +46,36 @@ public class VoipSender implements Runnable {
         
     }
     
+
+    
     //Create thread for receiver.
     public void start(){
         Thread thread = new Thread(this);
         thread.start();
+    }
+    
+    /*
+    *Plays the data recorded and sent.
+    *@throws IOException no blocks to play.
+    */
+    public void replay() throws IOException {
+        Iterator <byte[]> voiceItr = voiceVector.iterator();
+        
+        AudioPlayer player = null;
+        try {player = new AudioPlayer();}
+        catch (LineUnavailableException e){
+            e.printStackTrace();}
+        
+        
+        while(voiceItr.hasNext()){
+            player.playBlock(voiceItr.next());
+        }
+        
+        player.close();
+    }
+    //Call from duplex. Terminates main loop. 
+    public void stop(){
+        running = false;
     }
     
     @Override 
@@ -54,26 +85,36 @@ public class VoipSender implements Runnable {
         //Open socket to send from.
         sending_socket = new DatagramSocket();
         
-        //Keep on sending packets until false.
-        boolean running = true;
+        //Init, fill and send in the loop.
+        DatagramPacket packet;
+        byte[] block;
+        
+        voiceVector = new Vector<byte[]>();
+        
         while(running){         
-            /*
-            * Fill each block with audio data.
-            * Put block into a newly created packet.
-            * Keep on creating and sending packets while loop is running.
-            * Block size 512 bytes.
-            */
-            byte[] block = recorder.getBlock();
-            DatagramPacket packet = new DatagramPacket(block, block.length, clientIP, PORT);
-            sending_socket.send(packet);
-            //Indicate sent packet.
-            System.out.println("Packet sent.");
             
-            }
+            // Fill each block with audio data.
+            // Put block into a newly created packet.
+            // Keep on creating and sending packets while loop is running.
+            // Block size 512 bytes.
+            block = recorder.getBlock();
+            packet = new DatagramPacket(block, block.length, clientIP, PORT);
+            
+            //Add to use in replay() and count number of packets sent.
+            voiceVector.add(packet.getData());
+            
+            //Send.
+            sending_socket.send(packet);
+        }
         //Loop over, finish recording.
         recorder.close();
         //Close the socket.
         sending_socket.close();
+        System.out.printf("Sender is closed. Sent %d packets.\n", voiceVector.size());
+        
+        //Uncomment to replay sound recorded.
+        //replay();
+        
         } catch (IOException e){
             e.printStackTrace();
         }
